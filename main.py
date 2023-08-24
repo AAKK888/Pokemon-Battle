98333e75aec4e9862b3aca6fd84d94dc46e46483
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2023-08-24 18:37:31 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+class Char_1():
+
+    def __init__(self, sp_attack, sp_defence, speed, attack, defence, health,
+                 level):
+        self.sp_attack = sp_attack
+        self.sp_defence = sp_defence
+        self.speed = speed
+        self.attack = attack
+        self.defence = defence
+        self.health = health
+        self.level = level
+
+    def Attack(self, oppo_defence, base):
+        modifier = round(random.uniform(0.9, 1.2), 2)
+        damage_done = (
+            ((2 * self.level + 10) / 250 * self.attack / oppo_defence * base +
+             2) * modifier)
