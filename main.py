0f69d62de1437f682c64bfab9125790072393327
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2023-08-26 11:27:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import random
 
-class Char_1():
+
+class Char_1:
 
     def __init__(self, sp_attack, sp_defense, speed, attack, defense, max_health,
                  level, name):
         self.sp_attack = sp_attack
-        self.sp_defence = sp_defence
-    	self.speed = speed
+        self.sp_defence = sp_defense
+        self.speed = speed
         self.attack = attack
         self.defense = defense
         self.max_health = max_health
         self.level = level
         self.current_health = max_health
         self.is_knocked_out = False
-		self.name = name
-    def Attack(self, oppo_defence, base):
+        self.name = name
+
+    def attack(self, oppo_defence, base):
         modifier = round(random.uniform(0.9, 1.2), 2)
         damage_done = (
-            ((2 * self.level + 10) / 250 * self.attack / oppo_defence * base +
-             2) * modifier)
+                ((2 * self.level + 10) / 250 * self.attack / oppo_defence * base +
+                 2) * modifier)
 
     def lose_health(self, damage_taken):
-        if amount > self.current_health:  
-          self.current_health = 0
-          self.is_knocked_out = True 
+        if damage_taken > self.current_health:
+            self.current_health = 0
+            self.is_knocked_out = True
         else:
-          self.current_health -= amount 
+            self.current_health -= damage_taken
