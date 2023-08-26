fbac84ce2e33f1b7c1d211b1f57cf8ec4ab3ad21
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2023-08-26 12:24:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,13 @@ class Char_1:
     def attack(self, oppo_defence, base):
         modifier = round(random.uniform(0.9, 1.2), 2)
         damage_done = (((2 * self.level + 10) / 250 * self.attack / oppo_defence * base + 2) * modifier)
-        truefalse = (True, False, False, )
+        truefalse = (True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False,)
         chance = random.choice(truefalse)
+        if chance:
+            damage_done *= 1.5
+        else:
+            pass
+        return damage_done
 
     def lose_health(self, damage_taken):
         if damage_taken > self.current_health:
@@ -28,6 +33,7 @@ class Char_1:
             self.is_knocked_out = True
         else:
             self.current_health -= damage_taken
+        return self.current_health
 
     def knock_out(self):
         self.is_knocked_out = True
