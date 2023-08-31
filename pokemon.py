1d5a476971abb5fb75d1682c275937c4356cd49a
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2023-08-30 20:32:06 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -28,12 +28,25 @@ class Char_1:
             pass
         return damage_done
 
+     def attack(self, oppo_defence, base):
+        modifier = round(random.uniform(0.9, 1.2), 2)
+        damage_done = (((2 * self.level + 10) / 250 * self.attack / oppo_defence * base + 2) * modifier)
+        truefalse = (
+            True, False, False, False, False, False, False, False, False, False, False, False, False, False, False,
+            False,)
+        chance = random.choice(truefalse)
+        if chance:
+            damage_done *= 1.5
+        else:
+            pass
+        return damage_done
+
     def lose_health(self, damage_taken):
         if damage_taken > self.current_health:
             self.current_health = 0
             self.is_knocked_out = True
         else:
-            self.current_health -= damage_taken
+            self.current_health -= damage_taken 
         return self.current_health
 
     def knock_out(self):
