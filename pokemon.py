932f1dfa0f9e3cbdce6e019b11f7db0ae8489add
AAKK888 <89204848+AAKK888@users.noreply.github.com>
2023-09-16 15:39:19 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -34,11 +34,67 @@ class Pokemon:
             damage_done *= 1.5
         self.calculate_damage(atk_type, damage_done, oppo_type)
 
-    def sp_attack(self, atk_type, oppo_sp_defence, base, oppo_type):
+    def sp_attack(self, oppo_sp_defence, base, oppo_type):
         atk_type = "Normal"
-        if self.attack_1 == "Hyper_Beam":
-            atk_type = "Normal"
-            base = 120
+        if True:
+            if self.attack_1 == "Hyper_Beam":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Blast_Burn":
+                atk_type = "Normal"
+                base = 150
+            elif self.attack_1 == "Water_Shuriken":
+                atk_type = "Normal"
+                base = 110
+            elif self.attack_1 == "Thunder":
+                atk_type = "Normal"
+                base = 110
+            elif self.attack_1 == "Leaf_Storm":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Blizzard":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Flash_Cannon":
+                atk_type = "Normal"
+                base = 100
+            elif self.attack_1 == "Sludge_Bomb":
+                atk_type = "Normal"
+                base = 110
+            elif self.attack_1 == "Earth_Power":
+                atk_type = "Normal"
+                base = 100
+            elif self.attack_1 == "Hurricane":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Dazzling_Gleam":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Gust":
+                atk_type = "Normal"
+                base = 40
+            elif self.attack_1 == "Foul_Play":
+                atk_type = "Normal"
+                base = 95
+            elif self.attack_1 == "Aerial_Ace":
+                atk_type = "Normal"
+                base = 85
+            elif self.attack_1 == "Sunsteel_Strike":
+                atk_type = "Normal"
+                base = 120
+            elif self.attack_1 == "Dragon_Energy":
+                atk_type = "Normal"
+                base = 150
+            elif self.attack_1 == "Tri_Attack":
+                atk_type = "Normal"
+                base = 110
+            elif self.attack_1 == "Draco_Meteor":
+                atk_type = "Normal"
+                base = 110
+            elif self.attack_1 == "Clangorous_Soulblaze":
+                atk_type = "Normal"
+                base = 100
+
         modifier = round(random.uniform(0.75, 0.9), 2)
         damage_done = ((0.84 * self.sp_attack / oppo_sp_defence * base + 2) * modifier)
         truefalse = (
@@ -133,5 +189,46 @@ class Pokemon:
                 damage_done /= 2
             if oppo_type == "Steel":
                 damage_done /= 2
+        elif atk_type == "Ice":
+            if oppo_type == "Fire":
+                damage_done /= 2
+            if oppo_type == "Water":
+                damage_done /= 2
+            if oppo_type == "Grass":
+                damage_done *= 2
+            if oppo_type == "Ice":
+                damage_done /= 2
+            if oppo_type == "Ground":
+                damage_done *= 2
+            if oppo_type == "Flying":
+                damage_done *= 2
+            if oppo_type == "Dragon":
+                damage_done *= 2
+            if oppo_type == "Steel":
+                damage_done /= 2
+        elif atk_type == "Fighting":
+            if oppo_type == "Normal":
+                damage_done *= 2
+            if oppo_type == "Ice":
+                damage_done *= 2
+            if oppo_type == "Poison":
+                damage_done /= 2
+            if oppo_type == "Flying":
+                damage_done /= 2
+            if oppo_type == "Psychic":
+                damage_done /= 2
+            if oppo_type == "Ground":
+                damage_done /= 2
+            if oppo_type == "Rock":
+                damage_done *= 2
+            if oppo_type == "Ghost":
+                damage_done = 0
+            if oppo_type == "Dark":
+                damage_done *= 2
+            if oppo_type == "Steel":
+                damage_done *= 2
+            if oppo_type == "Fairy":
+                damage_done /= 2
+
         return damage_done
 
